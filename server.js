c6c260f42e310abe81a3b1f331f9fe9b77a9111d
Andy Durette <duretteandy@gmail.com>
2020-02-10 00:19:37 -0500

    Added service working functionality to store in cache for offline viewing v3

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9090;
 
 
 /******************************* MiddleWare  ****************************/
